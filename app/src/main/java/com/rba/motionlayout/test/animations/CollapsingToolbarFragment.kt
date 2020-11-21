614d1e10cf85b9a6eb77ca5f15a66229325c8654
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-20 20:30:26 -0500

    refactor: Hide activity toolbar

--- app/src/main/java/com/rba/motionlayout/test/animations/CollapsingToolbarFragment.kt
+++ app/src/main/java/com/rba/motionlayout/test/animations/CollapsingToolbarFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.AppBarLayout
 import com.rba.motionlayout.test.databinding.FragmentCollapsingToolbarBinding
+
 
 class CollapsingToolbarFragment : Fragment() {
 
@@ -24,4 +26,15 @@ class CollapsingToolbarFragment : Fragment() {
 
         return binding.root
     }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+    }
+
 }
